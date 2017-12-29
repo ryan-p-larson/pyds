de2a100e96c98bb670e18d6837aaf31756d91c73
Larson <rlrson@uiowa.edu>
2017-12-29 09:27:27 -0600

    Basic stack added.

---
+++ pyds/stack.py
@@ -0,0 +1,26 @@
+# Ryan Larson
+#
+#
+# stack.py
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def __repr__(self):
+        return str(self.items)
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
