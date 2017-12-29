da202113bc82270709181026e86a50b45158ae21
Larson <rlrson@uiowa.edu>
2017-12-29 16:09:57 -0600

    Add double node, and doubly linked list. Includes add_front/rear and remove_front/rear.

--- pyds/__init__.py
+++ pyds/__init__.py
@@ -1,5 +1,5 @@
 from .stack import Stack
 from .queue import Queue
-from .linkedlist import ListNode, LinkedList
+from .linkedlist import ListNode, LinkedList, DNode, DoubleLinkedList
 
-__ALL__ = [Stack, Queue, ListNode, LinkedList]
+__ALL__ = [Stack, Queue, ListNode, LinkedList, DNode, DoubleLinkedList]
--- pyds/linkedlist.py
+++ pyds/linkedlist.py
@@ -14,7 +14,7 @@ class ListNode:
         return ascii_data + ascii_tail
 
 class LinkedList:
-    def __init__(self, head):
+    def __init__(self, head=None):
         self.head = head
 
     def __repr__(self):
@@ -38,3 +38,84 @@ class LinkedList:
 
     def reverse(self):
         pass
+
+
+class DNode:
+    def __init__(self, data, prev=None, next_node=None):
+        self.data = data
+        self.prev = prev
+        self.next = next_node
+
+    def __repr__(self):
+        s0 = "<->" if self.prev else ":"
+        s1 = "({})".format(self.data)
+        s2 = "" if self.next else ":"
+        return s0 + s1 + s2
+
+class DoubleLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __repr__(self):
+        res = ""
+        curr = self.head
+        while curr:
+            res = res + str(curr)
+            curr = curr.next
+        return res
+
+    def is_empty(self):
+        return self.head is None
+
+    def size(self):
+        count = 0
+        if self.is_empty():
+            return count
+        curr = self.head
+        while curr:
+            count += 1
+            curr = curr.next
+        return count
+
+    def add_front(self, data):
+        # step 1: create node
+        node = DNode(data)
+        if not self.is_empty():
+            node.next = self.head
+            self.head.prev = node
+        #
+        else:
+            self.tail = node
+        self.head = node
+
+    def add_rear(self, data):
+        node = DNode(data)
+        if not self.is_empty():
+            node.prev = self.tail
+            self.tail.next = node
+        #
+        else:
+            self.head = node
+        self.tail = node
+
+    def remove_front(self):
+        if not self.is_empty():
+            temp = self.head
+            # check one node
+            if (temp.prev == temp.next):
+                self.head = None
+                self.tail = None
+            else:
+                self.head = temp.next
+                self.head.prev = None
+
+    def remove_rear(self):
+        if not self.is_empty():
+            temp = self.tail
+            if (temp.prev == temp.next):
+                self.head = None
+                self.tail = None
+            else:
+                self.tail = temp.prev
+                self.tail.next = None
